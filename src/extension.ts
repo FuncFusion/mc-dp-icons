6718b6a9bdffc619c409b56a4d27ecfc47027058
SuperAnt220 <zp-a@i.ua>
2023-12-19 14:30:13 +0200

    cleared up some code, better comments, beautiful

--- src/extension.ts
+++ src/extension.ts
@@ -1,54 +1,23 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as ThemeChange from './theme_change';
 import * as DynamicIcons from './dynamic_icons';
 
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// This function is called when the extension is activated
 export function activate(context: vscode.ExtensionContext) {
 	// console.log('Extension "mc-dp-icons" is now active!');
 
-  // Register the event listeners
-  context.subscriptions.push(
-    vscode.workspace.onDidChangeWorkspaceFolders(()=>{DynamicIcons.update(); ThemeChange.checkPackMcmeta();}),
-    vscode.workspace.onDidRenameFiles(()=>{DynamicIcons.update(); ThemeChange.checkPackMcmeta();}),
-    vscode.workspace.onDidDeleteFiles(()=>{DynamicIcons.update(); ThemeChange.checkPackMcmeta();}),
-    vscode.workspace.onDidCreateFiles(()=>{DynamicIcons.update(); ThemeChange.checkPackMcmeta();}),
-    vscode.workspace.onDidChangeTextDocument(DynamicIcons.update),
-    vscode.workspace.onDidChangeConfiguration(()=>{DynamicIcons.update(); ThemeChange.getDefaultIconTheme();})
-  );
-  
+	// Register the event listeners
+	context.subscriptions.push(
+		vscode.workspace.onDidChangeWorkspaceFolders(()=>{DynamicIcons.update(); ThemeChange.checkPackMcmeta();}),
+		vscode.workspace.onDidRenameFiles(()=>{DynamicIcons.update(); ThemeChange.checkPackMcmeta();}),
+		vscode.workspace.onDidDeleteFiles(()=>{DynamicIcons.update(); ThemeChange.checkPackMcmeta();}),
+		vscode.workspace.onDidCreateFiles(()=>{DynamicIcons.update(); ThemeChange.checkPackMcmeta();}),
+		vscode.workspace.onDidSaveTextDocument(DynamicIcons.update),
+		vscode.workspace.onDidChangeConfiguration(()=>{DynamicIcons.update(); ThemeChange.getDefaultIconTheme();})
+	);
 
-  // Get default Icon theme on startup
-  ThemeChange.getDefaultIconTheme();
-  
-  // Check for pack.mcmeta on startup
-  ThemeChange.checkPackMcmeta();
-
-  // Change load and tick mcfunction icons
-  DynamicIcons.update();
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('mc-dp-icons.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		ThemeChange.checkPackMcmeta();
-		vscode.window.showInformationMessage('Hello World from Datapack Icons!');
-	});
-
-
-	// let dynamicLoadTickChangeCommand = vscode.commands.registerCommand('mc-dp-icons.dynamicLoadTickChange', () => {
-	// 	dynamicLoadTickChange();
-	// 	vscode.window.showInformationMessage('Change tick and load functions icons');
-	// });
-
-	context.subscriptions.push(disposable);
-	// context.subscriptions.push(dynamicLoadTickChangeCommand);
-}
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
+	// Calling these functions on startup
+	ThemeChange.getDefaultIconTheme();
+	ThemeChange.checkPackMcmeta();
+	DynamicIcons.update();
+	}
--- src/theme_change.ts
+++ src/theme_change.ts
@@ -4,32 +4,35 @@ import { workspace } from 'vscode';
 let defaultIconTheme: string | undefined;
 
 export function getDefaultIconTheme() {
+		// Get the theme user set in the settings of the extension
 		let confDefaultIconTheme = workspace.getConfiguration().get<string>('mc-dp-icons.setDefaultIconTheme');
+		// If it's blank, then get current icon theme the user uses
 		if (confDefaultIconTheme === "") {
 			let gettingDefaultIconTheme = vscode.workspace.getConfiguration('workbench').get<string>('iconTheme');
+			// If the theme the user uses is mc-dp-icons, then set the defaultIconTheme to the one set in the settings of the extension
 			if (gettingDefaultIconTheme !== "mc-dp-icons") {
 				defaultIconTheme = gettingDefaultIconTheme;
 			}
 		} else {
 			defaultIconTheme = confDefaultIconTheme;
 		}
-		vscode.window.showInformationMessage('default icon theme is ' + defaultIconTheme);
 	}
-	
+
 export function checkPackMcmeta() {
+		// Get the configuration if the user wants to check for pack.mcmeta
 		const enableCheck = workspace.getConfiguration().get<boolean>('mc-dp-icons.enablePackMcmetaCheck');
+		// If the configuration is true, then start finding pack.mcmeta in the workspace
 		if (enableCheck) {
-		// Find the pack.mcmeta file in the workspace
+		// Fund pack.mcmeta in the workspace, excluding node_modules folder
 		vscode.workspace
 			.findFiles('**/pack.mcmeta', '**/node_modules/**')
 			.then((files) => {
 				if (files.length > 0) {
-					// Set the icon theme to the Datapack Icons theme
+					//If there is pack.mcmeta, then set the icon theme to the Datapack Icons theme
 					vscode.workspace.getConfiguration('workbench')
 						.update('iconTheme', 'mc-dp-icons', vscode.ConfigurationTarget.Workspace);
 				} else {
-					console.log('pack.mcmeta is not found');
-					// Reset the icon theme to the default one
+					// If there is no pack.mcmeta, set the icon theme to the default one
 					if (defaultIconTheme) {
 						vscode.workspace.getConfiguration('workbench')
 							.update('iconTheme', defaultIconTheme, vscode.ConfigurationTarget.Workspace);

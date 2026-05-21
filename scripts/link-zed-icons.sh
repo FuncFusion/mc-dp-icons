6c64289fde9e43233b5daf61737486adad0c6d48
amandin <amawwdin@gmail.com>
2026-05-21 12:48:42 +0600

    refactor: remove zed-specific files (moved to mc-dp-icons-zed)

--- scripts/link-zed-icons.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Creates symlink from zed/icons -> icons/
-# Run after cloning: bash scripts/link-zed-icons.sh
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-ROOT_DIR="$(dirname "$SCRIPT_DIR")"
-
-ln -sfn "$ROOT_DIR/icons" "$ROOT_DIR/zed/icons"
-echo "Created zed/icons -> ../icons symlink"
